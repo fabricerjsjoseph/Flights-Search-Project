9c1adf2aecc50d670ec1bb12b674aebcfb954118
fabricejoseph <fabricerjsjoseph@gmail.com>
2020-01-17 12:56:45 +0400

    FL02-C3-Reurns combined Sydney and Brisbane dataframe - tested and working

--- FL02_Sydney_Flights_Dataframe.py
+++ FL02_Sydney_Flights_Dataframe.py
@@ -1,29 +1,55 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
 
 import pandas as pd
 import glob
-from functools import reduce
-import seaborn as sns
-import matplotlib.pyplot as plt
 import numpy as np
 
 
 # Import all csv files in working directory
-csv_list=glob.glob(r"csv/*Sydney*.csv")
+csv_list=[glob.glob(r"csv/*Sydney*.csv")]
+csv_list.append(glob.glob(r"csv/*Brisbane*.csv"))
+
+# Initialise empty dataframes for Syd and Bne
+sydney_df=pd.DataFrame()
+brisbane_df=pd.DataFrame()
+
+# Load each csv to dataframe
+def csv_to_df(list,location_df):
+    for csv in list:
+        search_date_df=pd.read_csv(csv)
+        location_df=location_df.append(search_date_df)
+
+
+    # Concat departure and arrival time
+    location_df['Dep-Arrival Time']=location_df['Departure Time']+'-'+location_df['Arrival Time']
+    # Create a flight identifier
+    location_df['Flight ID']=location_df['Airline'] + '-'+ location_df['Dep-Arrival Time']
+
+    return location_df
+
+# generate sydney and brisbane dataframe
+sydney_df=csv_to_df(csv_list[0],sydney_df)
+brisbane_df=csv_to_df(csv_list[1],brisbane_df)
+
+
+# Create dictionary for flight paths
+flight_dict_sydney={'Emirates-4:45 pm-10:30 pm':'Emirates MRU-DBX-SYD',
+'Air Mauritius-10:15 pm-5:50 pm':'Air Mauritius MRU-PER-SYD option 1',
+'Air Mauritius-10:15 pm-10:40 pm':'Air Mauritius MRU-PER-SYD option 2',
+'Air Mauritius-8:40 pm-9:20 pm':'Air Mauritius MRU-SIN-SYD'}
+
+flight_dict_brisane={'Emirates-4:45 pm-10:40 pm':'Emirates MRU-DBX-BNE',
+'Air Mauritius-10:15 pm-7:25 pm':'Air Mauritius MRU-PER-BNE option 1',
+'Air Mauritius-10:15 pm-11:50 pm':'Air Mauritius MRU-PER-BNE option 2'}
+
+# Map Flight Path based on Flight ID
+sydney_df['Flight Path']=sydney_df['Flight ID'].map(flight_dict_sydney).fillna('Other-Sydney')
+brisbane_df['Flight Path']=brisbane_df['Flight ID'].map(flight_dict_brisane).fillna('Other-Brisbane')
+
 
 # Initialise as master dataframe
-master_df=pd.DataFrame()
+master_df=pd.concat([sydney_df,brisbane_df],ignore_index=True)
 
-# Load each csv to master dataframe
-for csv in csv_list:
-    df=pd.read_csv(csv)
-    master_df=master_df.append(df)
-
-# Remove AU$, commma and convert to AUD  
+# Remove AU$, commma and convert to AUD
 master_df['Current Price AUD']= master_df['Current Price AUD'].apply(lambda x: x.strip('AU$')).apply(lambda x: x.replace(',','')).astype('int64')
 
 # Remove brackets and leave only digits
@@ -40,19 +66,6 @@ master_df['Duration-Hours']=master_df['Duration-Hours'].astype('float64')
 master_df['Duration-Minutes']=master_df['Duration-Minutes'].astype('float64')
 master_df['Duration-Net Hours']=master_df['Duration-Hours']+master_df['Duration-Minutes'].apply(lambda x: round(x/60,1))
 
-# Create a flight identifier
-master_df['Flight ID']=master_df['Airline'] + '-'+ master_df['Dep-Arrival Time']
-
-# Create dictionary for flight paths
-flight_dict={'Emirates-4:45 pm-10:30 pm':'Emirates MRU-DBX-SYD',
-'Air Mauritius-10:15 pm-5:50 pm':'Air Mauritius MRU-PER-SYD option 1',
-'Air Mauritius-10:15 pm-10:40 pm':'Air Mauritius MRU-PER-SYD option 2',
-'Air Mauritius-8:40 pm-9:20 pm':'Air Mauritius MRU-SIN-SYD'}
-
-
-# Map Flight Path based on Flight ID
-master_df['Flight Path']=master_df['Flight ID'].map(flight_dict).fillna('Other')
-
 # Converting Search Date to a datetime object to enable sorting
 master_df['Search Date']=pd.to_datetime(master_df['Search Date'],format='%d-%m-%Y').dt.date
 
@@ -61,7 +74,6 @@ master_df.sort_values(by='Search Date',inplace=True)
 
 # Convert back to string
 master_df['Search Date']=master_df['Search Date'].astype(str)
-
 
 # Create copy of master_df
 lean_master_df=master_df.copy()
@@ -87,11 +99,3 @@ def generate_master_df_pivot():
 	master_df_pivot=master_df_pivot.stack().reset_index()
 	master_df_pivot.columns=['Flight Path','Search Date','No of Flights']
 	return master_df_pivot
-
-
-
-# In[ ]:
-
-
-
-
